9b08f494f9a3f8507ebb1f8a2ca36ed0277ef307
Daniel Nehring <nehrdani@gmail.com>
2016-01-10 17:30:21 +0100

    it's now a jQuery-Function

--- README.md
+++ README.md
@@ -1,7 +1,29 @@
 # jQuery-Modal
-* lightweight Modal Plugin for jQuery
-* easy to customise
+* lightweight Modal for jQuery
+* easy to customize
 * different animation styles
+* adds the $.Modal function to jQuery
 
-## Dependencies
-* jQuery >= v1.9
+## Dependencies:
+* jQuery >= v1.5
+
+## How To:
+simple:
+```javascript
+var modal = $.Modal("your modal content here");
+modal.open();
+```
+advanced:
+```javascript
+var modal = $.Modal("your modal content here", {
+  animation: "fade"
+});
+modal.open().done(function ()).fail(function ());
+```
+... or take a look at the examples.
+
+## Methods:
+* open() > opens the Modal and returns a promise
+* close([true, false], value) > closes the Modal and resolves / rejects the promise with the value;
+* $content > the content passed to the Modal as jQuery-Element
+* destroy() > destroys the Modal
--- js/modal.js
+++ js/modal.js
@@ -1,8 +1,12 @@
 (function ($) {
   "use strict";
 
-  var Modal = function (element, options) {
+  // using the jQuery namespace, extending it
+  $.Modal = Modal;
+
+  function Modal(element, options) {
     var defer;
+    var $element = $(element);
 
     // set Modal options
     options = $.extend({
@@ -12,17 +16,24 @@
     }, options);
 
     // create Wrapper Element
-    var $wrapper = $('<div></div>').addClass("modal-wrapper")
+    var $wrapper = $('<div></div>').addClass("jq-modal-wrapper")
     .appendTo("body");
 
     // create Backdrop Element
-    var $backdrop = $('<div></div>').addClass("modal-backdrop")
+    var $backdrop = $('<div></div>').addClass("jq-modal-backdrop")
     .appendTo($wrapper);
 
     // set to Modal Element
-    var $modal = $(element).addClass().addClass("modal-container")
-    .addClass("modal-" + options.animation) // apply the animation type
+    var $modal = $element.addClass().addClass("jq-modal-container")
     .appendTo($wrapper);
+
+    // if animation is set
+    if (options.animation) {
+      $modal.addClass("jq-modal-animate jq-modal-animate-" + options.animation); // apply the animation type
+      $backdrop.addClass("jq-modal-animate");
+    }
+
+    /////////////////////////////////////////////
 
     // Modal open Method
     function modalOpen() {
@@ -31,7 +42,7 @@
       // close on click modal overlay
       if (options.modalClose) {
         $backdrop.on("click.modal", function (event) {
-          modalClose(false);
+          modalClose(false, "closed");
         });
       }
 
@@ -39,22 +50,30 @@
       if (options.closeOnKey) {
         $(window).on("keyup.modal", function (event) {
           if (event.which === 27) {
-            modalClose(false);
+            modalClose(false, "closed");
           }
         });
       }
 
-      // setting class to modal-visible, starting animations shows the Modal
-      $backdrop.addClass("modal-visible");
-      $wrapper.addClass("modal-visible");
-      $modal.addClass("modal-visible");
+      // setting class to on, starting animations shows the Modal
+      $backdrop.addClass("on");
+      $wrapper.addClass("on");
+      $modal.addClass("on");
 
       // return promise
-      return defer;
+      return defer.promise();
     }
 
     // Modal close Method
     function modalClose(status, data) {
+      // unbind close events
+      $wrapper.off();
+      $(window).off("keyup.modal");
+
+      // removing class on, starting animations, hides the Modal
+      $backdrop.removeClass("on");
+      $wrapper.removeClass("on");
+      $modal.removeClass("on");
 
       // resolve or reject promise
       if (status) { // true
@@ -62,15 +81,6 @@
       } else { // false
         defer.reject(data);
       }
-
-      // unbind close events
-      $wrapper.off();
-      $(window).off("keyup.modal");
-
-      // removing class modal-visible, starting animations, hides the Modal
-      $backdrop.removeClass("modal-visible");
-      $wrapper.removeClass("modal-visible");
-      $modal.removeClass("modal-visible");
     }
 
     // destroys Modal if not used anymore
@@ -88,31 +98,8 @@
     return {
       close: modalClose,
       open: modalOpen,
+      $content: $element,
       destroy: _destroy
     };
-  };
-
-  $.fn.modal = function () {
-    var args = arguments;
-    return this.each(function () {
-      if ($(this).data("modal")) {
-        if (typeof args[0] === "string") {
-          try  {
-            $(this).data("modal")[args[0]](args[1]);
-          }
-          catch(err) {
-            console.error("modal has no method " + args[0]);
-          }
-        } else {
-          console.error("modal plugin already initialized");
-        }
-      } else {
-        if (typeof args[0] === "object" || typeof args[0] === "undefined") {
-          $(this).data("modal", new Modal(this, args[0]));
-        } else {
-          console.error(args[0] + " is not a function");
-        }
-      }
-    });
-  };
-})(jQuery || window.jQuery);
+  }
+})(jQuery);
